b8b4177b9cfee04b432e00eae2ee9bf00c947c86
comical-tag-7436 <shawtynow2008@gmail.com>
2019-10-27 03:26:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function destructivelyAppendKitten(Name){
   kittens.push('Ralph');
   return kittens;
 }
+function destructivelyPrependKitten(name){
+  kittens.unshift('Ralph');
+  return kittens
+}
